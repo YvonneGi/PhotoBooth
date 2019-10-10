545cccdffd60c2e47a4081de01a51ed444711d89
YvonneGi <giryvonne@gmail.com>
2019-10-10 20:20:39 +0200

    create Django admin

--- photos/admin.py
+++ photos/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from .models import Images,Location,Category
 
 # Register your models here.
+admin.site.register(Images)
+admin.site.register(Location)
+admin.site.register(Category)
