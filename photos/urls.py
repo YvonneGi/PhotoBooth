1644aafa9bec66b8f6fce788cc430b70e65fcbae
YvonneGi <giryvonne@gmail.com>
2019-10-11 11:51:32 +0200

    create template to display all images

--- photos/urls.py
+++ photos/urls.py
@@ -1,8 +1,14 @@
 from django.conf.urls import url
 from . import views
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns=[
     url('^$',views.welcome,name = 'welcome'),
     url('^today/$', views.display_image, name='Today'),
+    url(r'^photo/(\d+)', views.single_photo, name='singlePhoto'),
+    url(r'^all/$', views.all_images, name='allImages')
    
 ]
+if settings.DEBUG:
+    urlpatterns+= static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
--- photos/views.py
+++ photos/views.py
@@ -12,3 +12,11 @@ def welcome(request):
 def display_image(request):
     date = dt.date.today()
     return render(request, 'today.html', {"date": date})
+
+def single_photo(request, photo_id):
+    photo = Images.objects.get(id=photo_id)
+    return render(request, 'single_image.html', {'photo': photo})
+
+def all_images(request):
+    images = Images.get_images()
+    return render(request, 'all_images.html', {"images": images})
