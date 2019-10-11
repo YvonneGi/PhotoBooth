eb8f952073214808f32d7ab3f20d38daaddec8a8
YvonneGi <giryvonne@gmail.com>
2019-10-11 16:17:02 +0200

    update views function for getting images by ID

--- photos/views.py
+++ photos/views.py
@@ -11,15 +11,15 @@ def welcome(request):
 
 def display_image(request):
     date = dt.date.today()
-    return render(request, 'today.html', {"date": date})
+    return render(request, 'all-photos/today.html', {"date": date})
 
 def single_photo(request, photo_id):
-    photo = Images.objects.get(id=photo_id)
-    return render(request, 'single_image.html', {'photo': photo})
+    images = Images.objects.get(id=photo_id)
+    return render(request, 'all-photos/single_image.html', {'images': images})
 
 def all_images(request):
     images = Images.get_images()
-    return render(request, 'all_images.html', {"images": images})
+    return render(request, 'all-photos/all_images.html', {"images": images})
 
 def search_images(request):
     if 'photos' in request.GET and request.GET['photos']:
@@ -27,7 +27,7 @@ def search_images(request):
         searched_photo = Images.search_by_cat_name(search_term)
         photos = Images.objects.filter(name=searched_photo).all()
         message = f"{search_term}"
-        return render(request, 'search_image.html', {"message": message, "photos": searched_photo})
+        return render(request, 'all-photos/search_image.html', {"message": message, "photos": searched_photo})
     else:
         message = 'You haven\'t searched for any photos.'
-        return render(request, 'search_image.html', {"message": message})
+        return render(request, 'all-photos/search_image.html', {"message": message})
